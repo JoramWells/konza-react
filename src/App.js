37b0f2c009f153662f6ae6b7c984289b68293708
unknown <jorammanoah1@gmail.com>
2021-05-02 12:29:39 -0700

    Added componets folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import './App.css';
 
 async function fetchData(){
-  await axios.get('/todos').then(res=>console.log(res)).catch(err=>console.log(err))
+  await axios.get('/api/todos').then(res=>console.log(res.data)).catch(err=>console.log(err))
 }
 
 function App() {
