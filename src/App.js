4ef83bf6c56e1f0c91c9bc3b65eabe32890a118a
unknown <jorammanoah1@gmail.com>
2021-05-02 12:00:30 -0700

    Proxy add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+async function fetchData(){
+  await axios.get('/todos').then(res=>console.log(res)).catch(err=>console.log(err))
+}
 
+function App() {
+  useEffect(() => {
+    fetchData()
+    return () => {
+      
+    }
+  }, [])
+  return (
+    <div>
+      <h1>hahahaaa</h1>
     </div>
+
   );
 }
 
